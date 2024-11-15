1293b3eaa688853a3d608b0a0b72feef06b73c09
Jakub Tulek <jakub.tulek@esmosk.sk>
2024-11-15 15:51:40 +0100

    Implement basic menu

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::{thread, time::Duration};
+
 use console::{style, Term};
+use dialoguer::{theme::ColorfulTheme, Select};
 use tkpr::WorkManager;
 use console::Style;
 
@@ -6,7 +9,15 @@ fn main() {
     let term = Term::stdout();
     term.hide_cursor();
     term.clear_screen();
-    let work_manager = WorkManager::new(None);
+    let mut work_manager = WorkManager::new(None);
+
+    work_manager.new_session("123".into(), "SAWO".into());
+    work_manager.new_session("124".into(), "SAWO".into());
+    work_manager.new_session("125".into(), "SAWO".into());
+    work_manager.new_session("126".into(), "SAWO".into());
+
+    thread::sleep(Duration::from_secs(5));
+
     let orange = Style::new().yellow();
     term.write_line("Welcome to");
     term.write_line(
@@ -24,9 +35,14 @@ fn main() {
     if !sessions.is_empty() {
         term.write_line("Active Sessions:");
         for session in sessions {
-            term.write_line(&format!("{}: {}", session.identifier, session.project));
+            term.write_line(&format!("{}: {} -- {}", session.identifier, session.project, session.current_duration()));
         }
     } else {
         term.write_line(&format!("You currently have {} active sessions.", style("NO").magenta()));
     }
+
+    loop {
+        let selection = Select::with_theme(&ColorfulTheme::default()).with_prompt("What would you like to do?").default(0).item("End Session").item("End Session Without Saving").item("Exit").interact().unwrap();
+    }
+
 }
