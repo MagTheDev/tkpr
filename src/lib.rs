8721845891b75d3e79a9ece18fa3d191bd7b4b8a
Jakub Tulek <jakub.tulek@esmosk.sk>
2024-11-15 15:36:41 +0100

    Implement active sessions

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,19 @@
 use std::{collections::HashMap, path::PathBuf};
-use chrono::{DateTime, Duration, Utc};
+use chrono::{DateTime, Duration, Local, Utc};
 use rusqlite::{params, Connection};
 
 
 #[derive(Debug, Clone, Hash)]
 pub struct WorkRecord {
     pub id: i32,
-    pub date: DateTime<Utc>,
+    pub date: DateTime<Local>,
     pub description: String,
     pub duration: Duration,
     pub project: String
 }
 
 impl WorkRecord {
-    pub fn new(id: i32, date: DateTime<Utc>, description: String, duration: Duration, project: String) -> Self {
+    pub fn new(id: i32, date: DateTime<Local>, description: String, duration: Duration, project: String) -> Self {
         Self { id, date, description, duration, project }
     }
 }
@@ -22,7 +22,8 @@ impl WorkRecord {
 
 pub struct WorkManager {
     conn: Connection,
-    records: HashMap<i32, WorkRecord>
+    records_cache: HashMap<i32, WorkRecord>,
+    active_sessions: HashMap<String, ActiveWorkSession>
 }
 
 impl WorkManager {
@@ -45,14 +46,15 @@ impl WorkManager {
         "#, ()).expect("Error initializing database");
         Self {
             conn: connection,
-            records: HashMap::default(),
+            records_cache: HashMap::default(),
+            active_sessions: HashMap::default()
         }
     }
     pub fn create_work_record(&self, record: WorkRecord) -> Result<usize, rusqlite::Error> {
         self.create_work_record_raw(record.date, record.duration, record.description, record.project)
     }
 
-    pub fn create_work_record_raw(&self, date: DateTime<Utc>, duration: Duration, description: String, project: String) -> Result<usize, rusqlite::Error> {
+    pub fn create_work_record_raw(&self, date: DateTime<Local>, duration: Duration, description: String, project: String) -> Result<usize, rusqlite::Error> {
         self.conn.execute(r#"--sql
             INSERT INTO work_records (
                 date, description, duration, project
@@ -70,7 +72,7 @@ impl WorkManager {
 
     pub fn get_work_record_by_id(&mut self, id: i32) -> Option<WorkRecord> {
 
-        if let Some(record) = self.records.get(&id).cloned() {
+        if let Some(record) = self.records_cache.get(&id).cloned() {
             return Some(record)
         }
 
@@ -88,12 +90,12 @@ impl WorkManager {
             project: row.get(4).ok()?
         };
 
-        self.records.insert(record.id, record.clone());
+        self.records_cache.insert(record.id, record.clone());
         Some(record)
     }
 
     pub fn get_all_cached_records(&self) -> Vec<WorkRecord> {
-        return self.records.values().cloned().collect();
+        return self.records_cache.values().cloned().collect();
     }
 
     pub fn get_all_records(&self) -> Vec<WorkRecord> {
@@ -123,5 +125,70 @@ impl WorkManager {
         }
 
         records
+    }
+
+    pub fn delete_record_by_id(&self, id: i32) -> Option<()> {
+
+        let mut record_query = self.conn.prepare(r#"--sql
+            DELETE FROM work_records WHERE id = ?1
+        "#).unwrap();
+
+        record_query.execute([&id]).ok()?;
+
+        Some(())
+    }
+
+    pub fn new_session(&mut self, ident: String, project: String) -> ActiveWorkSession {
+        let session = ActiveWorkSession::new(ident, None, project);
+        self.active_sessions.insert(session.identifier.clone(), session.clone());
+        session
+    }
+
+    pub fn end_session(&mut self, ident: String, description: Option<String>) -> Option<WorkRecord> {
+        let session = self.active_sessions.remove(&ident)?;
+        let record = session.end(description);
+        self.create_work_record(record.clone()).ok()?;
+        Some(record)
+    }
+
+    pub fn end_session_without_saving(&mut self, ident: String, description: Option<String>) -> Option<WorkRecord> {
+        let session = self.active_sessions.remove(&ident)?;
+        let record = session.end(description);
+        self.records_cache.insert(record.id, record.clone());
+        Some(record)
+    }
+
+    pub fn get_active_sessions(&self) -> Vec<ActiveWorkSession> {
+        self.active_sessions.values().cloned().collect()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ActiveWorkSession {
+    pub identifier: String,
+    pub start: DateTime<Local>,
+    pub project: String
+}
+
+impl ActiveWorkSession {
+    pub fn new(identifier: String, start: Option<DateTime<Local>>, project: String) -> Self {
+        let start = match start {
+            Some(start) => start,
+            None => Local::now(),
+        };
+        Self { identifier, start, project }
+    }
+
+    pub fn end(self, description: Option<String>) -> WorkRecord {
+
+        let end_time = Local::now();
+        let work_duration = self.start - end_time;
+
+        let description = match description {
+            Some(description) => description,
+            None => self.identifier,
+        };
+
+        WorkRecord::new(-1, self.start, description, work_duration, self.project)
     }
 }
