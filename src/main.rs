43c14ebbaa21f14bf7d812d9269568b37d30d782
Jakub Tulek <jakub.tulek@esmosk.sk>
2024-11-15 15:37:58 +0100

    Create a basic menu

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
+use console::{style, Term};
 use tkpr::WorkManager;
+use console::Style;
 
 fn main() {
-    let wm = WorkManager::new(None);
+    let term = Term::stdout();
+    term.hide_cursor();
+    term.clear_screen();
+    let mut work_manager = WorkManager::new(None);
+    let orange = Style::new().yellow();
+    term.write_line("Welcome to");
+    term.write_line(
+        &orange.apply_to(r#"  _______ _                _  __                         
+ |__   __(_)              | |/ /                         
+    | |   _ _ __ ___   ___| ' / ___  ___ _ __   ___ _ __ 
+    | |  | | '_ ` _ \ / _ \  < / _ \/ _ \ '_ \ / _ \ '__|
+    | |  | | | | | | |  __/ . \  __/  __/ |_) |  __/ |   
+    |_|  |_|_| |_| |_|\___|_|\_\___|\___| .__/ \___|_|   
+                                        | |              
+                                        |_|              "#).to_string()
+    );
+
+    let sessions = work_manager.get_active_sessions();
+    if sessions.len() > 0 {
+        term.write_line("Active Sessions:");
+        for session in sessions {
+            term.write_line(&format!("{}: {}", session.identifier, session.project));
+        }
+    } else {
+        term.write_line(&format!("You currently have {} active sessions.", style("NO").magenta()));
+    }
 }
